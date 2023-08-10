c1484bfd488b88fd984ee806acc8b892da066895
Sterling <sterlingcobb@gmail.com>
2023-08-09 22:39:22 -0600

    chore: add doctor form and fixed a few things

--- src/app/mypets/page.tsx
+++ src/app/mypets/page.tsx
@@ -19,7 +19,7 @@ export default function MyPets() {
         </div>
       </div>
       <div className="">
-        <div className="grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-2">
           {pets.length ? (
             pets.map((pet) => <PetDisplayTile key={pet.name} name={pet.name} />)
           ) : (
