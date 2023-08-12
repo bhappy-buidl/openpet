0fb78623587104123028728bfeb7aef35168372e
Samkbe <samkbe@gmail.com>
2023-08-12 16:16:00 -0500

    Added Logo

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,9 +1,16 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
+import openPetLogo from "../app/openpet-logo.png";
 
 export function TopBar() {
   return (
-    <div className="flex flex-row justify-end p-4">
-      <ConnectButton />
+    <div className="w-full flex">
+      <div className="flex flex-row flex-grow place-content-between p-4 items-center max-w-screen-xl mx-auto">
+        <a href="/">
+          <Image width={50} height={50} alt="OpenPet Logo" src={openPetLogo} />
+        </a>
+        <ConnectButton />
+      </div>
     </div>
   );
 }
