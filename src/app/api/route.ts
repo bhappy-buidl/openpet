d1fc930464ef87e3060131c6a83c9b9aa3fe88c0
Sterling <sterlingcobb@gmail.com>
2023-08-13 00:51:26 -0600

    image upload working

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,58 +1,6 @@
 import Bundlr from "@bundlr-network/client";
 import { NextResponse } from "next/server";
 
-// function mkBundlr() {
-//   const bundlr = new Bundlr(
-//     "https://devnet.bundlr.network",
-//     "matic",
-//     process.env.PRIVATE_KEY,
-//     {
-//       providerUrl: "https://matic-mumbai.chainstacklabs.com",
-//     }
-//   );
-
-//   return bundlr;
-// }
-
-// async function uploadPetData(file: Buffer) {
-//   const bundlr = mkBundlr();
-
-//   const uploadTx = await bundlr.uploadFile(file);
-//   console.log(`Pet data uploaded URL=https://arweave.net/${uploadTx.id}`);
-
-//   return uploadTx.id;
-// }
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
-
-// export async function POST(request: Request) {
-//   try {
-//     const formData = await request.formData();
-
-//     console.log(formData);
-
-//     // Ensure that the 'photo' field contains a file
-//     // if (file instanceof File) {
-//     //   const buffer = new Buffer(await file.arrayBuffer());
-//     //   const transactionId = await uploadPetData(buffer);
-
-//     //   // Respond with the transaction ID
-//     //   return NextResponse.json({ transactionId });
-//     // }
-
-//     return NextResponse.json({ formData });
-
-//     // return Error("Photo is missing or invalid");
-//   } catch (error) {
-//     console.error(error);
-//     return Error("Failed to upload pet data");
-//   }
-// }
-
 function mkBundlr() {
   const bundlr = new Bundlr(
     "https://devnet.bundlr.network",
@@ -85,8 +33,13 @@ export async function POST(request: Request) {
   const fileBuffer = Buffer.from(await imageFile.arrayBuffer());
 
   // Upload the Buffer using the bundlr.upload function
+
+  // todo, detect the image type and store the correct type
   try {
-    const response = await bundlr.upload(fileBuffer);
+    // @ts-ignore tag types are correct, idk why it's being that way
+    const response = await bundlr.upload(fileBuffer, [
+      { name: "Content-Type", value: "image/jpeg" },
+    ]);
     const arweaveUrl = `https://arweave.net/${response.id}`;
     console.log(arweaveUrl);
     return NextResponse.json({ transactionId: response.id });
